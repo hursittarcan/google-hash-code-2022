18d0bff34da8118399080a2d172edfaae50d5a7c
hursittarcan <hursittarcan@gmail.com>
2023-02-22 12:27:43 +0100

    bug fixed

--- practice-round/index.js
+++ practice-round/index.js
@@ -1,13 +1,23 @@
 const { readFileSync } = require('fs');
 
 function readFile(filename) {
-    return readFileSync(filename, 'utf-8');
+    const contents = readFileSync(filename, 'utf-8');
+    return contents.split(/\r?\n/);
 }
 
 let input = readFile('sample.txt');
 
-let potential_customers = input[0];
-
-
+let potential_customers = input[0].toString();
 
 console.log("Potential customers: " + potential_customers);
+console.log()
+
+for (let i = 0; i < potential_customers * 2; i++) {
+    //let liked_ingredients = input[i];
+    console.log("customer: " )
+    console.log("Likes: " + input[i+1]);
+    console.log("Dislikes: " + input[i+2]);
+    i++
+    console.log()
+}
+
